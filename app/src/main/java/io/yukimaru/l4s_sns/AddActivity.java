3e6d089840f7bb8a91fc68259cfdeaff3df29c1c
yuki1124 <yuki.cherrysnowman@gmail.com>
2018-09-19 01:27:26 +0900

    two

---
+++ app/src/main/java/io/yukimaru/l4s_sns/AddActivity.java
@@ -0,0 +1,13 @@
+package io.yukimaru.l4s_sns;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add);
+    }
+}
--- app/src/main/java/io/yukimaru/l4s_sns/MainActivity.java
+++ app/src/main/java/io/yukimaru/l4s_sns/MainActivity.java
@@ -1,13 +1,40 @@
 package io.yukimaru.l4s_sns;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements CustomAdapter.OnLikeClickListener {
+    ListView listView;
+    FloatingActionButton add_button;
+
+    public CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        add_button = (FloatingActionButton) findViewById(R.id.add_button);
+
+        adapter = new CustomAdapter(this, 0, new ArrayList<ClipData.Item>());
+        adapter.setOnLikeClickListener(this);
+        listView.setAdapter(adapter);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
+        adapter.addAll(getSampleData());
     }
 }
